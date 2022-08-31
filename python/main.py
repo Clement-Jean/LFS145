49e74f5bd20706ac0bc28385c00cd90c417b3116
Clement <clement.jean@epitech.eu>
2022-08-31 16:17:14 +0800

    add user schema

--- python/main.py
+++ python/main.py
@@ -1,12 +1,36 @@
-import proto.account_pb2 as pb
+import sys
+import proto.account_pb2 as account_pb
+import proto.user_pb2 as user_pb
 
 def account():
-    return pb.Account(
+    return account_pb.Account(
         id = 42,
         name = "linus_torvalds",
         is_verified = True,
         follow_ids = [0, 1]
     )
 
+def user():
+    message = user_pb.User()
+    message.id = 42
+    message.name = "Linus Torvalds"
+    message.friends.add(id=0, name="Clement Jean")
+    message.friends.add(id=1, name="Someone else")
+    return message
+
 if __name__ == "__main__":
-    print(account())
+    if len(sys.argv) != 2:
+        print("Usage: main.py [account]")
+        exit()
+
+    switcher = {
+        "account": account,
+        "user": user,
+    }
+    fn = switcher.get(sys.argv[1], None)
+
+    if not fn:
+        print(f"Unknown function: \"{sys.argv[1]}\"")
+        exit()
+
+    print(fn())
