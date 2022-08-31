b2e0e7b853dce45e970bec82365f675b3dd7966c
Clement <clement.jean@epitech.eu>
2022-08-31 16:52:33 +0800

    instantiate an enum

--- python/main.py
+++ python/main.py
@@ -1,13 +1,14 @@
 import sys
 import proto.account_pb2 as account_pb
 import proto.user_pb2 as user_pb
+import proto.product_pb2 as product_pb
 
 def account():
     return account_pb.Account(
-        id = 42,
-        name = "linus_torvalds",
-        is_verified = True,
-        follow_ids = [0, 1]
+        id=42,
+        name="linus_torvalds",
+        is_verified=True,
+        follow_ids=[0, 1]
     )
 
 def user():
@@ -18,16 +19,35 @@ def user():
     message.follows.add(id=1, name="Clement Jean")
     return message
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: main.py [account]")
-        exit()
+def user2():
+    return user_pb.User(
+        id=42,
+        name="Linus Torvalds",
+        follows=[
+            user_pb.User(id=0, name="Linux Foundation"),
+            user_pb.User(id=1, name="Clement Jean"),
+        ]
+    )
 
-    switcher = {
+def product():
+    return product_pb.Product(
+        id=42,
+        product_type = product_pb.ProductType.PANTS # 1
+    )
+
+if __name__ == "__main__":
+    fns = {
         "account": account,
         "user": user,
+        "user2": user2,
+        "product": product
     }
-    fn = switcher.get(sys.argv[1], None)
+
+    if len(sys.argv) != 2:
+        print(f"Usage: main.py [{'|'.join(fns)}]")
+        exit()
+
+    fn = fns.get(sys.argv[1], None)
 
     if not fn:
         print(f"Unknown function: \"{sys.argv[1]}\"")
