f481179da645ab7d4e971c6b7c0b979efb4ead41
Clement <clement.jean@epitech.eu>
2022-09-01 11:06:18 +0800

    add any instantiation and replace " by '

--- python/main.py
+++ python/main.py
@@ -4,11 +4,12 @@ import proto.user_pb2 as user_pb
 import proto.product_pb2 as product_pb
 import proto.phone_book_pb2 as phone_book_pb
 import proto.login_pb2 as login_pb
+import google.protobuf.any_pb2 as any_pb
 
 def account():
     return account_pb.Account(
         id=42,
-        name="linus_torvalds",
+        name='linus_torvalds',
         is_verified=True,
         follow_ids=[0, 1]
     )
@@ -16,19 +17,19 @@ def account():
 def user():
     return user_pb.User(
         id=42,
-        name="Linus Torvalds",
+        name='Linus Torvalds',
         follows=[
-            user_pb.User(id=0, name="Linux Foundation"),
-            user_pb.User(id=1, name="Clement Jean"),
+            user_pb.User(id=0, name='Linux Foundation'),
+            user_pb.User(id=1, name='Clement Jean'),
         ]
     )
 
 def user2():
     message = user_pb.User()
     message.id = 42
-    message.name = "Linus Torvalds"
-    message.follows.add(id=0, name="Linux Foundation")
-    message.follows.add(id=1, name="Clement Jean")
+    message.name = 'Linus Torvalds'
+    message.follows.add(id=0, name='Linux Foundation')
+    message.follows.add(id=1, name='Clement Jean')
     return message
 
 def product():
@@ -40,20 +41,20 @@ def product():
 def phone_book():
     return phone_book_pb.PhoneBook(
         phones={
-            "Linus Torvald": "11111111",
-            "Clement Jean": "22222222"
+            'Linus Torvald': '11111111',
+            'Clement Jean': '22222222'
         }
     )
 
 def phone_book2():
     book = phone_book_pb.PhoneBook()
-    book.phones["Linus Torvald"] = "11111111"
-    book.phones["Clement Jean"] = "22222222"
+    book.phones['Linus Torvald'] = '11111111'
+    book.phones['Clement Jean'] = '22222222'
     return book
 
 def login_error():
     result = login_pb.LoginResult()
-    result.error = "The username or password are not correct"
+    result.error = 'The username or password are not correct'
     return result
 
 def login_success():
@@ -61,26 +62,36 @@ def login_success():
         token = login_pb.Token()
     )
 
-if __name__ == "__main__":
+def any_value():
+    num = 7
+
+    return [
+        any_pb.Any(value=num.to_bytes(2, byteorder='big')),
+        any_pb.Any(value=num.to_bytes(2, byteorder='little')),
+        any_pb.Any(value=bytes('test', 'utf-8'))
+    ]
+
+if __name__ == '__main__':
     fns = {
-        "account": account,
-        "user": user,
-        "user2": user2,
-        "product": product,
-        "phone": phone_book,
-        "phone2": phone_book2,
-        "logine": login_error,
-        "logins": login_success,
+        'account': account,
+        'user': user,
+        'user2': user2,
+        'product': product,
+        'phone': phone_book,
+        'phone2': phone_book2,
+        'logine': login_error,
+        'logins': login_success,
+        'any': any_value
     }
 
     if len(sys.argv) != 2:
-        print(f"Usage: main.py [{'|'.join(fns)}]")
+        print(f'Usage: main.py [{"|".join(fns)}]')
         sys.exit()
 
     fn = fns.get(sys.argv[1], None)
 
     if not fn:
-        print(f"Unknown function: \"{sys.argv[1]}\"")
+        print(f'Unknown function: \'{sys.argv[1]}\'')
         sys.exit()
 
     print(fn())
